fa328b0390236e05b0ee8f89df276d6d1e62e23f
Kurtgon <gongmateos@gmail.com>
2020-11-15 14:38:41 +0100

    header fix

---
+++ assets/js/header.js
@@ -0,0 +1,13 @@
+const openbtn = document.getElementById("openbtn");
+const closebtn = document.getElementById("closebtn")
+
+
+openbtn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    document.getElementById("menu").style.width= "250px";
+})
+
+closebtn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    document.getElementById("menu").style.width= "0";
+})
